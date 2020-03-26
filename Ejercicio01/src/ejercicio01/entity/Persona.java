bb7e60004340af7cf9f89566e096024a73e693a5
pabvidcal <pabvidcal@gmail.com>
2020-03-26 19:42:56 +0100

    Correction ejercicio001

--- Ejercicio01/src/ejercicio01/entity/Persona.java
+++ Ejercicio01/src/ejercicio01/entity/Persona.java
@@ -1,6 +1,4 @@
-package Ejercicio01;
-
-import java.util.Scanner;
+package ejercicio01.entity;
 
 public class Persona {
 
@@ -8,20 +6,13 @@ public class Persona {
     int edad;
     float altura;
 
-    String consulta_nome() {
+    public String consultaNome() {
         return nombre;
     }
 
-    void cambia_nome(String nom) {
+    public void cambiaNome(String nom) {
         nombre = nom;
     }
 
-    public static void main(String[] args) {
 
-        Persona nova_persoa = new Persona();
-        Scanner entrada_nome = new Scanner(System.in);
-        System.out.printf("Introduce o nome da persoa: ");
-        String nom = entrada_nome.nextLine();
-        System.out.printf("Nome: " + nom);
-    }
 }
